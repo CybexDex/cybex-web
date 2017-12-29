e86d0e6371e3e847473ac980fc9fd774cb62f264
ldwformat <ldwformat@gmail.com>
2017-12-29 13:52:09 +0800

    add node api schema

--- app/api/apiConfig.js
+++ app/api/apiConfig.js
@@ -21,7 +21,9 @@ const DEFAULT_WS_NODE = TEST_NODE;
 const DEFAULT_FAUCET = "http://118.190.209.196:3000";
 
 class ApiNode {
-    constructor(url, name) {
+    constructor(host, name) {
+        let schema = location && location.protocol.indexOf("https") !== -1 ? "wss://" : "ws://";
+        let url = schema + host;
         this.url = url;
         this.location = name;
     }
@@ -29,15 +31,15 @@ class ApiNode {
 
 
 const WS_NODE_LIST = [
-    new ApiNode("ws://111.231.200.95/", "Chengdo"),
-    new ApiNode("ws://118.190.209.196/", "Qingdao"),
-    new ApiNode("ws://47.96.163.7/", "Hangzhou"),
-    new ApiNode("ws://120.79.11.96/", "Shenzhen"),
-    new ApiNode("ws://hongkong-01.cybex.io/", "Hongkong"),
-    new ApiNode("ws://tokyo-01.cybex.io/", "Tokyo"),
-    new ApiNode("ws://singapore-01.cybex.io/", "Singapore"),
-    new ApiNode("ws://korea-01.cybex.io/", "Korea"),
-    new ApiNode("ws://usa-01.cybex.io/", "USA")
+    new ApiNode("111.231.200.95/", "Chengdo"),
+    new ApiNode("118.190.209.196/", "Qingdao"),
+    new ApiNode("hanzhou.cybex.io/", "Hangzhou"),
+    new ApiNode("120.79.11.96/", "Shenzhen"),
+    new ApiNode("hongkong.cybex.io/", "Hongkong"),
+    new ApiNode("tokyo-01.cybex.io/", "Tokyo"),
+    new ApiNode("singapore-01.cybex.io/", "Singapore"),
+    new ApiNode("korea-01.cybex.io/", "Korea"),
+    new ApiNode("usa-01.cybex.io/", "USA")
 ];
 
 export const settingsAPIs = {
