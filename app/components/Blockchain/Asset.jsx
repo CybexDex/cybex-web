78bdb6cf83834ffa8ff6a253c669268774db2fc6
ldwformat <ldwformat@gmail.com>
2018-03-13 20:08:04 +0800

    remove helpcontent on asset

--- app/components/Blockchain/Asset.jsx
+++ app/components/Blockchain/Asset.jsx
@@ -248,14 +248,11 @@ class Asset extends React.Component {
 
         return (
             <div style={{ overflow: "visible" }}>
-                <HelpContent
-                    path={"assets/" + asset.symbol}
-                    alt_path="assets/Asset"
-                    section="summary"
-                    symbol={(prefix || "") + name}
-                    description={desc}
-                    issuer={issuerName}
-                />
+                <div className="asset-header">
+                    <h3>{(prefix || "") + name}</h3>
+                    <p>{desc}</p>
+                    <p>Issued By: {issuerName}</p>
+                </div>
                 {short_name ? <p>{short_name}</p> : null}
                 <a style={{ textTransform: "uppercase" }} href={`${__HASH_HISTORY__ ? "#" : ""}/market/${asset.symbol}_${preferredMarket}`}><Translate content="exchange.market" /></a>
             </div>
