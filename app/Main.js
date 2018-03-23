65eeca6184f8929141d2a3bae3daf95aecc7771a
ldwformat <ldwformat@gmail.com>
2018-03-23 17:47:08 +0800

    add some translate

--- app/Main.js
+++ app/Main.js
@@ -1,5 +1,6 @@
+console.debug = () => void (0);
 if (!window.Intl) { // Safari polyfill
-    	require.ensure(["intl"], require => {
+    require.ensure(["intl"], require => {
         window.Intl = require("intl");
         Intl.__addLocaleData(require("./assets/intl-data/en.json"));
         require("index.js");
