b0d15d6751c240f4b2bb1589e3541a523550baff
lin <profchenmu@me.com>
2018-07-05 00:06:12 +0800

    fix

--- app/components/Eo/index.tsx
+++ app/components/Eo/index.tsx
@@ -18,20 +18,27 @@ import BindToChainState from "../Utility/BindToChainState";
 import AccountInfo from "../Account/AccountInfo";
 import { connect } from "alt-react";
 import AccountStore from "stores/AccountStore";
+import moment from "moment";
+
 
 class EO extends React.Component<any, any> {
   // nestedRef;
   constructor(props) {
     super(props);
     this.state = {
+      data:[[]],
       offset:0
     };
   }
 
   componentDidMount(){
-    console.log(this.props)
-    fetchJson.fetchJsonList((data)=>{
-      this.setState({data: data.result});
+    fetchJson.fetchJsonList(this.state.offset, (data)=>{
+      let newDate = this.state.data;
+      newDate[0] = data.result
+      this.setState({
+        offset: this.state.offset+4,
+        data: newDate
+      });
     });
     fetchJson.fetchBanner((res)=>{
       this.setState({bannerData: res.result});
@@ -48,9 +55,20 @@ class EO extends React.Component<any, any> {
     this.reactSwipe.prev();
   }
   
-
+  addMore() {
+    fetchJson.fetchJsonList(this.state.offset, (data)=>{
+      let newDate = this.state.data;
+      newDate[this.state.offset/4] = data.result
+      this.setState({
+        offset: this.state.offset+4,
+        data: newDate
+      });
+    });
+  }
 
   render() {
+    let hideMoreBtn = 'block';
+
     const data = this.state.data || [];
     const bannerData = this.state.bannerData || [];
     const swipeOptions = {
@@ -113,47 +131,75 @@ class EO extends React.Component<any, any> {
       
         }
       <div className="container">
-      <div className="waterfall">
-      {data.map((e,i)=>{
+      {data.map((f,j) => {
+        if(f.length == 0 || f.length < 4){
+          hideMoreBtn = 'none';
+        }
+        if(f.length<4 && f.length !== 0){
+          hideMoreBtn = 'none';
+          let comingSoonLength = 4 - f.length;
+          for(let i = 0; i< comingSoonLength; i++){
+            f.push({comingSoon: true});
+          }
+        }
+
+        return (
+          <div className="waterfall">
+      {f.map((e,i)=>{
         let percent = e.current_percent*100;
         percent = percent.toFixed(2);
-        let showPercent = `${percent>100?100:percent}%`
+        let showPercent = `${percent>100?100:percent}%`;
+        let endAt = moment(e.end_at);
+        let now = moment();
+        let remainStr = `${0-endAt.diff(now,'days')} ${moment(moment(e.end_at).valueOf() - moment().valueOf()).format('hh:mm:ss')}`
         return(
-          <div className="pin" key={i}>
-            <img src={logo_demo} width={100} height={100} />
-            <h3 className="title">{e.name}<span>
-            {e.status == 'ok'? (
-              <p>[<Translate content="EIO.ok" />]</p>
-            ):(
-              (e.status == 'pre')? (
-                <p>[<Translate content="EIO.pre" />]</p>
-              ):(
-                e.status == 'finish'? (
-                  <p>[<Translate content="EIO.finish" />]</p>
-                ):(
-                  <p>[<Translate content="EIO.pause" />]</p>
-                )
-              )
-            )}
-            
-            </span></h3>
-            <p>{e.adds_detail}</p>
-            <Link to={`/ieo/detail/${e.id}`}>
-            <div className="button primery-button"><Translate content="EIO.Details" /></div>
-            </Link>
-            <div className="info-item">
-              <div className="percent">
-                <div className="percent-in" style={{width: showPercent}}></div>
+          e.comingSoon==true?(
+              <div className="pin" key={i}>
+                <h3>coming soon</h3>
               </div>
-              <div className="info-text">{`${percent}%`}</div>
+            ):(
+            <div className="pin" key={i}>
+              <img src={logo_demo} width={100} height={100} />
+              <h3 className="title">{e.name}<span>
+              {e.status == 'ok'? (
+                <p className="status-label">[<Translate content="EIO.ok" />]</p>
+              ):(
+                (e.status == 'pre')? (
+                  <p className="status-label">[<Translate content="EIO.pre" />]</p>
+                ):(
+                  e.status == 'finish'? (
+                    <p className="status-label">[<Translate content="EIO.finish" />]</p>
+                  ):(
+                    <p className="status-label">[<Translate content="EIO.pause" />]</p>
+                  )
+                )
+              )}
+              
+              </span></h3>
+              <p>{e.adds_detail}</p>
+              <Link to={`/ieo/detail/${e.id}`}>
+              <div className="button primery-button"><Translate content="EIO.Details" /></div>
+              </Link>
+              <div className="info-item">
+                <div className="percent">
+                  <div className="percent-in" style={{width: showPercent}}></div>
+                </div>
+                <div className="info-text">{`${percent}%`}</div>
+              </div>
+              <p>{remainStr}</p>
             </div>
-          </div>
+          )
+          
+        )
+
+
+      })}
+      </div>
         )
       })}
       
-
       </div>
-      </div>
+      <div style={{display: hideMoreBtn}} onClick={this.addMore.bind(this)}>Add More</div>
       </div>
     );
   }
