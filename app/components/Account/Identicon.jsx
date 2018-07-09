df5b8d16ef322356844c4824d2e2c86cfe2b62f4
profchenmu <profchenmu@profchenmus-MacBook-Pro.local>
2018-07-09 11:23:56 +0800

    restore original file

--- app/components/Account/Identicon.jsx
+++ app/components/Account/Identicon.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as PropTypes from "prop-types";
 
 import sha256 from "js-sha256";
-// import jdenticon from "jdenticon";
+import jdenticon from "jdenticon";
 
 var canvas_id_count = 0;
 
@@ -39,17 +39,17 @@ class Identicon extends React.Component {
   }
 
   repaint() {
-    // if (this.props.account) jdenticon.updateById(this.canvas_id);
-    // else {
-    //   let ctx = this.refs.canvas.getContext("2d");
-    //   ctx.fillStyle = "rgba(100, 100, 100, 0.5)";
-    //   let size = ctx.canvas.width;
-    //   ctx.clearRect(0, 0, size, size);
-    //   ctx.fillRect(0, 0, size, size);
-    //   ctx.clearRect(0 + 1, 0 + 1, size - 2, size - 2);
-    //   ctx.font = `${size}px sans-serif`;
-    //   ctx.fillText("?", size / 4, size - size / 6);
-    // }
+    if (this.props.account) jdenticon.updateById(this.canvas_id);
+    else {
+      let ctx = this.refs.canvas.getContext("2d");
+      ctx.fillStyle = "rgba(100, 100, 100, 0.5)";
+      let size = ctx.canvas.width;
+      ctx.clearRect(0, 0, size, size);
+      ctx.fillRect(0, 0, size, size);
+      ctx.clearRect(0 + 1, 0 + 1, size - 2, size - 2);
+      ctx.font = `${size}px sans-serif`;
+      ctx.fillText("?", size / 4, size - size / 6);
+    }
   }
 
   componentDidMount() {
