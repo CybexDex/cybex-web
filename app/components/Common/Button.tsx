e84c310c5d7b614a550dc8da373dc8a8ff2775c1
ldwformat <ldwformat@gmail.com>
2018-07-11 16:31:03 +0800

    adjust some btn style

--- app/components/Common/Button.tsx
+++ app/components/Common/Button.tsx
@@ -58,6 +58,12 @@ let Button = class extends React.Component<ButtonProps, any> {
       },
       ":active": {
         background: Colors.$colorOrange
+      },
+      ":disabled": {
+        background: Colors.$colorGreyLightWhite,
+        cursor: "not-allowed",
+        color: Colors.$colorGreyLight,
+        opacity: "1"
       }
     },
     secondary: {
