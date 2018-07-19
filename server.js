9a7ad6d1bd1fa051e93dd56d6b51de1bce23b6c4
ldwformat <ldwformat@gmail.com>
2018-07-20 00:27:20 +0800

    adjust eto test api

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var feathers = require("feathers");
 const https = require("https");
 const fs = require("fs");
 const expressproxy = require('express-http-proxy');
-let proxyUrl = 'https://ieo-apitest.nbltrust.com/';
+let proxyUrl = 'https://ieo-apitest.cybex.io/';
 // https://ieo-apitest.nbltrust.com/api/cybex/projects?limit=4&offset=0
 
 var ProgressPlugin = require("webpack/lib/ProgressPlugin");
