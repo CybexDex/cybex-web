13fc6beef76270f3ad0f5c400ae68ebd075e4978
ldwformat <ldwformat@gmail.com>
2018-08-03 19:57:09 +0800

    Adjust ETO addresss

--- app/services/IEOService.ts
+++ app/services/IEOService.ts
@@ -2,7 +2,7 @@ export const API_URL = __DEV__
   ? "https://ieo-apitest.cybex.io/api/cybex/"
   : __TEST__ 
     ? "https://ieo-apitest.cybex.io/api/cybex/"
-    : "https://ieo.cybex.io/api/cybex/";
+    : "//eto.cybex.io/api/cybex/";
 
 export const PERSONAL_TRADE = `${API_URL}trade/list?cybex_name=alec-2216&page=1&limit=1`
 export const getTradeUrl = account => `${API_URL}trade/list?cybex_name=${account}&page=1&limit=1000`
